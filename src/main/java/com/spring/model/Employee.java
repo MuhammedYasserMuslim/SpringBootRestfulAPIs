ec6a0faefb5fa699c21efa68f6eb13d5ceb33e80
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-04 00:01:08 +0300

    CreateModel

---
+++ src/main/java/com/spring/model/Employee.java
@@ -0,0 +1,25 @@
+package com.spring.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    private double salary;
+}
